72e41a3619963e24e1115b617c66917a6cb1adc9
rhumaine <romain.demay56@hotmail.fr>
2024-06-18 23:23:55 +0200

    amélioration menu mobile

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -30,49 +30,115 @@ const UlNav = styled.ul`
   width: 100%;
   text-align: center;
 
-   @media (max-width: 649px) {
+  @media (max-width: 649px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    right: -100vw;
+    bottom: 0;
+    height:100vh;
+    padding: 2rem;
+    background: #000;
+    border-radius:0;
+    margin:0;
+    visibility:hidden;
+    transition: all .8s ease-out;
+
+    .showNav &{
+      right: 0;
+      visibility:visible;
+    }
+
+    & li:after{
+      content: "";
+      display:block;
+      margin: 0 auto;
+      width: 3vw;
+      height:1px;
+      background:#fff;
+    }
+
+    & li:last-child:after{
+      display:none;
+    }
+
+    li > ul {
+      display: flex;
+      border-radius: 20px;
+      position: relative;
+      top:0;
+      margin-top: -30px!important;
       flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      position: fixed;
-      right: -100vw;
-      bottom: 0;
-      height:100vh;
-      padding: 2rem;
-      background: #000;
-      border-radius:0;
-      margin:0;
-      visibility:hidden;
-      transition: all .8s ease-out;
+      background: transparent;
+      left: 50px;
+    }
 
-      .showNav &{
-        right: 0;
-        visibility:visible;
-      }
+     & li.open > ul {
+      display: block;
+    }
 
-      & li:after{
-        content: "";
-        display:block;
-        margin: 0 auto;
-        width: 3vw;
-        height:1px;
-        background:#fff;
-      }
+    li {
+    position: relative;
 
-      & li:last-child:after{
-        display:none;
+    ul {
+      display: none;
+      padding: 0;
+      list-style: none;
+
+      li {
+        padding: 10px;
+
+        a {
+          color: white;
+        }
       }
     }
-`
+  }
+  }
+
+  @media (min-width: 650px){
+    li:hover > ul {
+      display: flex;
+      margin-top: 20px;
+      border-radius: 20px;
+    }
+
+    li {
+    position: relative;
+
+    ul {
+      display: none;
+      position: absolute;
+      top: 100%;
+      left: 0;
+      background-color: rgba(50, 15, 19, 0.8);
+      padding: 0;
+      list-style: none;
+
+      li {
+        padding: 10px;
+
+        a {
+          color: white;
+        }
+      }
+    }
+  }
+  }
+`;
+
 const NavLink = styled(RouterNavLink)`
-  padding: 10px;
+  padding: 20px 10px;
   color: white;
   text-decoration: none;
   font-size: 1em;
   font-weight: 400;
+
   &:hover {
     text-decoration: none;
   }
+
   &.active {
     color: #FD8686; /* Couleur différente pour le lien actif */
   }
@@ -81,7 +147,24 @@ const NavLink = styled(RouterNavLink)`
     display:block;
     padding:1.5rem;
     font-size: 5vw;
+  }
+`;
 
+const Anchor = styled.a`
+  padding: 20px 10px;
+  color: white;
+  text-decoration: none;
+  font-size: 1em;
+  font-weight: 400;
+
+  &:hover {
+    text-decoration: none;
+  }
+
+  @media (max-width: 649px) {
+    display:block;
+    padding:1.5rem;
+    font-size: 5vw;
   }
 `;
 
@@ -95,7 +178,7 @@ const ButtonNavBar = styled.button`
   right:2rem;
 
   @media (min-width: 650px) {
-     display:none;
+    display:none;
   }
 `;
 
@@ -109,8 +192,7 @@ const SpanBurger = styled.span`
     background:#fff;
     transition: all .5s ease-in-out;
   }
-  
-  
+
   .showNav &{
     background:transparent;
   }
@@ -139,24 +221,44 @@ const SpanBurger = styled.span`
 `;
 
 function Nav() {
-  const [showLinks, setShowLinks] = useState(false)
+  const [showLinks, setShowLinks] = useState(false);
+  const [openMenu, setOpenMenu] = useState(null);
 
   const handleShowLinks = () => {
-    setShowLinks(!showLinks)
+    setShowLinks(!showLinks);
   }
 
   const handleLinkClick = () => {
     setShowLinks(false);
+    setOpenMenu(null);
+  };
+
+  const handleMenuClick = (menu, event) => {
+    event.preventDefault();
+    setOpenMenu(openMenu === menu ? null : menu);
   };
 
   return (
-    <Navbar className={ showLinks ? 'showNav' : ''}>
+    <Navbar className={showLinks ? 'showNav' : ''}>
       <UlNav>
         <li><NavLink to="/" end onClick={handleLinkClick}>Accueil</NavLink></li>
         <li><NavLink to="/miniature" onClick={handleLinkClick}>Miniatures</NavLink></li>
-        <li><NavLink to="/commission" onClick={handleLinkClick}>Commission</NavLink></li>
-        <li><NavLink to="/emotes"onClick={handleLinkClick}>Emotes</NavLink></li>
-        <li><NavLink to="/logoBanniere"onClick={handleLinkClick}>Logo & Bannières</NavLink></li>
+        <li><NavLink to="/commission" onClick={handleLinkClick}>Commissions</NavLink></li>
+        <li className={openMenu === 'twitch' ? 'open' : ''}>
+          <Anchor href="/" onClick={(e) => handleMenuClick('twitch', e)}>Twitch</Anchor>
+          <ul>
+            <li><NavLink to="/logoBanniere/emote" onClick={handleLinkClick}>Emotes</NavLink></li>
+            <li><NavLink to="/logoBanniere/badge" onClick={handleLinkClick}>Badges</NavLink></li>
+            <li><NavLink to="/logoBanniere/badge" onClick={handleLinkClick}>Overlay</NavLink></li>
+          </ul>
+        </li>
+        <li className={openMenu === 'da' ? 'open' : ''}>
+          <Anchor href="/" onClick={(e) => handleMenuClick('da', e)}>D.A</Anchor>
+          <ul>
+            <li><NavLink to="/logoBanniere/emote" onClick={handleLinkClick}>Logo</NavLink></li>
+            <li><NavLink to="/logoBanniere/badge" onClick={handleLinkClick}>Bannière</NavLink></li>
+          </ul>
+        </li>
         <li><NavLink to="mailto:luciole.pro@outlook.fr" onClick={handleLinkClick}>Contact</NavLink></li>
       </UlNav>
       <ButtonNavBar onClick={handleShowLinks}>
@@ -164,6 +266,6 @@ function Nav() {
       </ButtonNavBar>
     </Navbar>
   );
-};
+}
 
 export default Nav;
