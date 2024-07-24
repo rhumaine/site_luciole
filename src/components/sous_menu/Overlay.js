1d6b79030fabbd052d6f9a4420fb524685f679eb
rhumaine <romain.demay56@hotmail.fr>
2024-07-24 22:50:08 +0200

    gif overlay

--- src/components/sous_menu/Overlay.js
+++ src/components/sous_menu/Overlay.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from '../Header';
-import overlayRaton from '../../img/portfolio/overlay.gif';
+import overlayRaton from '../../img/portfolio/overlay.jpg';
+import overlayAnimee from '../../img/portfolio/overlay.gif';
 
 const Image = styled.img`
   width: 100%;
@@ -46,6 +47,7 @@ const Overlay = () => {
         <h2 className='title-section'>Overlay</h2>
         <StyledDiv>
           <Image src={overlayRaton} alt="Overlay Raton" />
+          <Image src={overlayAnimee} alt="Overlay animé" />
         </StyledDiv>
     </div>
   );
